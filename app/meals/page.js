1c59f84c8528acfebb13ccc8f60ba6725d6b6cf7
Sowmya G S <sowmyags1066@gmail.com>
2024-07-17 11:46:10 +0530

    nav links content

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choos your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">share your recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
