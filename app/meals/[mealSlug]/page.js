6d6def6afc01642ae1a4df59c4ff36a4852a9944
Sowmya G S <sowmyags1066@gmail.com>
2024-07-18 17:37:29 +0530

    Rendering Details via Dyanamic routes

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,37 @@
-export default function MealSlug() {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getData } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default function MealSlug({ params }) {
+  const meal = getData(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
     </>
   );
 }
